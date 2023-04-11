1caa9fe660e1331fe7ca56d80ddaebee35707b67
amgarton47 <amgb2018@mymail.pomona.edu>
2023-04-11 02:43:40 -0500

    added some backend logging

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const path = require("path");
+const morgan = require("morgan");
 const express = require("express");
 
 const port = 3000;
 const app = express();
 app.use(express.static("public"));
+app.use(morgan("dev"));
 
 app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
